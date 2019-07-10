5d22df0f9ae7ae10740a86d8f9fdadb70ca4ec88
Menara Lintang Was <menaralintang7@gmail.com>
2019-07-10 23:32:04 +0700

    Edit table, Add button.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,24 +1,33 @@
+@extends
 <div class="container">
         <div class="col p-3 main">
-            <p>Hello, admin</p>
-            
-      <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">No.</th>
-              <th scope="col">Name</th>
-              <th scope="col">Qty</th>
-              <th scope="col">Link</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Aki istimewa</td>
-              <td>12</td>
-              <td>bit.ly/bla</td>
-            </tr>
-          </tbody>
-        </table>
+          <h4>Hello, admin</h4>
+          
+        <table class="table table-borderless" style="background: #fff">
+            <thead>
+              <tr>
+                <th scope="col">No.</th>
+                <th scope="col">Name</th>
+                <th scope="col">Qty</th>
+                <th scope="col">Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Aki istimewa</td>
+                <td>12</td>
+                <td>bit.ly/bla</td>
+                <td>
+                  <button class="btn btn-success" href="#">Edit</button>
+                  <button class="btn btn-danger" href=#>Remove</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="btn btn-info float-right" href="#"> 
+              <a href="#" class="text-light">Add Product</a>
+          </div>
         </div>
-</div>
+         
+      </div>
